346517f1dc46a507848afa560657be46355c6638
Kenny <kennysk81@gmail.com>
2018-11-29 12:17:51 -0600

    Se agrego clase java

---
+++ prueba1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class prueba1
+{
+    public static void main (String [] args)
+    {
+        double pi;
+        double rad, op,lon;
+        Scanner leerDato= new Scanner (System.in);
+        System.out.println('\u000C');
+        System.out.println("Ingresa el valor del radio de un círculo");
+        rad=leerDato.nextInt();
+        pi=3.1416;
+        lon= rad + rad;
+        op= pi * ( rad * rad );
+        System.out.println("La longitud es : " + lon);
+        System.out.println("El área del círculo es: " + op);
+    }
+}
